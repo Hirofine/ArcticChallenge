c904f65ccd481138f752cf93d55274d824568b81
JB <48212794+josuebecerra7@users.noreply.github.com>
2021-12-07 21:16:22 +0100

    dummy test

--- weatherapp/app.js
+++ weatherapp/app.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const Web3 = require("web3")
-const web3 = new Web3("http://localhost:8545")
+
+//const web3 = new Web3("http://localhost:8545")
+const web3 = new Web3("https://cloudflare-eth.com")
+
+web3.eth.getBlockNumber(function (error, result) {
+    console.log(result)
+	})
+
 
 
 app.get('/', (req, res) => {
