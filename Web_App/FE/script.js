1df19688ad0446db0e734b8e585516de8d2df350
Hirofine <43345721+Hirofine@users.noreply.github.com>
2021-12-07 21:33:50 +0100

    print api

--- Web_App/FE/script.js
+++ Web_App/FE/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
-  console.log("please be working")
-
+  console.log("please be working");
+  var res;
+  var test = 10;
+  $.getJSON('https://jsonplaceholder.typicode.com/todos', function(data){
+    document.getElementById("json").textContent = JSON.stringify(data, undefined, 2);
+  });
   
-
 });
