c8dd918371640ef96744f02fa71bd3ac5461e881
JB <48212794+josuebecerra7@users.noreply.github.com>
2021-12-07 21:38:07 +0100

    web3

--- Web_App/FE/script.js
+++ Web_App/FE/script.js
@@ -5,5 +5,15 @@ $(document).ready(function () {
   $.getJSON('https://jsonplaceholder.typicode.com/todos', function(data){
     document.getElementById("json").textContent = JSON.stringify(data, undefined, 2);
   });
-  
+
+  var Web3 = require("web3");
+  //const web3 = new Web3("http://localhost:8545");
+ const web3 = new Web3("https://cloudflare-eth.com");
+	
+async function getBlockNumber() {
+	  const latestBlockNumber = await web3.eth.getBlockNumber();
+	  console.log(latestBlockNumber);
+	  return latestBlockNumber;
+	}	
+	getBlockNumber();
 });
