67dccbbfb82da480b359a0b47898a04c72451b21
Hirofine <43345721+Hirofine@users.noreply.github.com>
2021-12-07 19:49:45 +0100

    bullshit not working shits

--- Web_App/FE/scipt.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
-});
---
+++ Web_App/FE/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  console.log("please be working")
+
+  
+
+});
