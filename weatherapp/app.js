e26b816a0f5f2455453f12d99f55c430b0a9033b
JB <48212794+josuebecerra7@users.noreply.github.com>
2021-12-07 19:29:38 +0100

    Update app.js

--- weatherapp/app.js
+++ weatherapp/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 const Web3 = require("web3")
+const web3 = new Web3("http://localhost:8545")
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
