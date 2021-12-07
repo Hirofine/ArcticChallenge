44623e2242afbd314c9f38b0d0813cf10cfa5c8b
JB <48212794+josuebecerra7@users.noreply.github.com>
2021-12-07 22:28:57 +0100

    trying to require

--- weatherapp/app.js
+++ weatherapp/app.js
@@ -2,6 +2,23 @@ const express = require('express')
 const app = express()
 const port = 3000
 const Web3 = require("web3")
+var weather_url = "https://api.met.no/weatherapi/locationforecast/2.0/classic?lat=64.749&lon=20.9594";
+
+const axios = require('axios');
+
+// Make a request for a user with a given ID
+axios.get(weather_url)
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
 
 //const web3 = new Web3("http://localhost:8545")
 const web3 = new Web3("https://cloudflare-eth.com")
