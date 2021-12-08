77db354494279a9862a36d20bdfe913696fe78fb
JB <48212794+josuebecerra7@users.noreply.github.com>
2021-12-08 19:29:55 +0100

    Update brocker.js

--- Brocker/brocker.js
+++ Brocker/brocker.js
@@ -1,10 +1,30 @@
 const Web3 = require("web3")
-var weather_url = "https://api.met.no/weatherapi/locationforecast/2.0/classic?lat=64.749&lon=20.9594";
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const port = new SerialPort('COM3', { baudRate: 9600 });
+const parser = port.pipe(new Readline({ delimiter: '\n' }));
+var json_data;
+var output;
 
 const web3 = new Web3("http://172.27.63.201:7545")
-web3.eth.getBlockNumber(function (error, result) {
-    console.log(result)
-	})
+var EthereumSession = web3.eth.contract();
+var ethereumSessionInstance = EthereumSession.at(" ");
 
+web3.eth.defaultAccount = web3.eth.accounts[0];
 
+// Read the port data
+port.on("open", () => {
+    console.log('serial port open');
+});
+
+parser.on('data', data =>{
+//  console.log('got word from arduino:', data);
+    json_data = data.split(" ");
+    output = [["celsius",parseInt(json_data[0])],["percentage",parseInt(json_data[1])],["celsius",parseInt(json_data[2])],Date.now()]
+    console.log(output);
+    ethereumSessionInstance.setval(output);
+    console.log(ethereumSessionInstance.getval());
+// Here we need to send output to the blockchain, how? ask romain
+
+});
     
