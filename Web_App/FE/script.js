1b9093b7b575ddd33e7e019cf0e382682f30ad6a
Hirofine <43345721+Hirofine@users.noreply.github.com>
2021-12-09 01:03:39 +0100

    integration should be good in FE

--- Web_App/FE/script.js
+++ Web_App/FE/script.js
@@ -1,15 +1,17 @@
+
+
 $(document).ready(function () {
     //var Web3 = require('web3');
     let conAddress = "0xEE4fb72da9AeaEc2D8A1E4a21fC9f3E0F507Dd3E"
-   let abi = '[{"inputs":[],"name":"getval","outputs":[{"components":[{"internalType":"int256","name":"t","type":"int256"},{"internalType":"uint256","name":"h","type":"uint256"},{"internalType":"int256","name":"hI","type":"int256"},{"internalType":"uint256","name":"timestamp","type":"uint256"}],"internalType":"structCrudApp.Temp[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"components":[{"internalType":"int256","name":"t","type":"int256"},{"internalType":"uint256","name":"h","type":"uint256"},{"internalType":"int256","name":"hI","type":"int256"},{"internalType":"uint256","name":"timestamp","type":"uint256"}],"internalType":"structCrudApp.Temp","name":"recTemp","type":"tuple"}],"name":"setval","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"weatherdb","outputs":[{"internalType":"int256","name":"t","type":"int256"},{"internalType":"uint256","name":"h","type":"uint256"},{"internalType":"int256","name":"hI","type":"int256"},{"internalType":"uint256","name":"timestamp","type":"uint256"}],"stateMutability":"view","type":"function"}]';
+    let abi = '[{"inputs":[],"name":"getval","outputs":[{"components":[{"internalType":"int256","name":"t","type":"int256"},{"internalType":"uint256","name":"h","type":"uint256"},{"internalType":"int256","name":"hI","type":"int256"},{"internalType":"uint256","name":"timestamp","type":"uint256"}],"internalType":"structCrudApp.Temp[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"components":[{"internalType":"int256","name":"t","type":"int256"},{"internalType":"uint256","name":"h","type":"uint256"},{"internalType":"int256","name":"hI","type":"int256"},{"internalType":"uint256","name":"timestamp","type":"uint256"}],"internalType":"structCrudApp.Temp","name":"recTemp","type":"tuple"}],"name":"setval","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"weatherdb","outputs":[{"internalType":"int256","name":"t","type":"int256"},{"internalType":"uint256","name":"h","type":"uint256"},{"internalType":"int256","name":"hI","type":"int256"},{"internalType":"uint256","name":"timestamp","type":"uint256"}],"stateMutability":"view","type":"function"}]';
 
     let account = "0xd5a84c66d68001aBbe27569A1df9C83D4f1d1F4c";
 
     //const web3 = new Web3("http://172.27.63.201:7545");
     const web3 = new Web3("http://192.168.137.216:7545");
-    
-    let deploy_contract = new web3.eth.Contract(JSON.parse(abi),conAddress);
-    deploy_contract.methods.getval().call();
+
+    let deploy_contract = new web3.eth.Contract(JSON.parse(abi), conAddress);
+   
 
     let latestKnownBlockNumber = -1;
     let blockTime = 5000;
@@ -25,8 +27,6 @@ $(document).ready(function () {
             let transactionReceipt = await web3.eth.getTransactionReceipt(transactionHash);
             transaction = Object.assign(transaction, transactionReceipt);
             console.log("Transaction: ", transaction);
-            var value = await deploy_contract.methods.getval().call();
-            console.log(value);
             // Do whatever you want here
             //$.getJSON('{"userId": 1, "id": 1,"title": "delectus aut autem","completed": false}', function (data) {
             //document.getElementById("json").textContent += "<li>" + JSON.stringify(transaction, undefined, 2) + "</li>";
@@ -55,67 +55,58 @@ $(document).ready(function () {
 
     checkCurrentBlock()
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    var temp = [
-        { y: '2014', a: 50 },
-        { y: '2015', a: 65 },
-        { y: '2016', a: 50 },
-        { y: '2017', a: 75 },
-        { y: '2018', a: 80 },
-        { y: '2019', a: 90 },
-        { y: '2020', a: 100 },
-        { y: '2021', a: 115 },
-        { y: '2022', a: 120 },
-        { y: '2023', a: 145 },
-        { y: '2024', a: 160 }
-    ],
-        config = {
-            data: temp,
-            xkey: 'y',
-            ykeys: ['a'],
-            labels: ['Temperature °C'],
-            fillOpacity: 0.6,
-            hideHover: 'auto',
-            behaveLikeLine: true,
-            resize: true,
-            pointFillColors: ['#ffffff'],
-            pointStrokeColors: ['#de6fa1'],
-            lineColors: ['#de6fa1'],
-            barColors: ['#de6fa1']
-        };
-    config.element = 'temp-chart';
-    Morris.Line(config);
+    async function draw_graphs() {
+        var value = await deploy_contract.methods.getval().call();
+        console.log("value in graph")
+        console.log(value);
+        var pre_temp = [];
+        var pre_hum = [];
+        for (var i = 2; i < value.length; i++) {
+            pre_temp[i-2] = { y: parseInt(value[i].timestamp), a: parseInt(value[i].t) };
+            pre_hum[i-2] = {y:parseInt(value[i].timestamp), a:parseInt(value[i].h)}
+        }
+        console.log("Pre temp = ");
+        console.log(pre_temp);
+        var temp =pre_temp,
+            config = {
+                data: temp,
+                xkey: 'y',
+                ykeys: ['a'],
+                labels: ['Temperature °C'],
+                fillOpacity: 0.6,
+                hideHover: 'auto',
+                behaveLikeLine: true,
+                resize: true,
+                pointFillColors: ['#ffffff'],
+                pointStrokeColors: ['#de6fa1'],
+                lineColors: ['#de6fa1'],
+                barColors: ['#de6fa1']
+            };
+        config.element = 'temp-chart';
+        Morris.Line(config);
 
-    var hum = [
-        { y: '2014', a: 50 },
-        { y: '2015', a: 65 },
-        { y: '2016', a: 50 },
-        { y: '2017', a: 75 },
-        { y: '2018', a: 80 },
-        { y: '2019', a: 90 },
-        { y: '2020', a: 10 },
-        { y: '2042', a: 12 },
-        { y: '2022', a: 69 },
-        { y: '2023', a: 42 },
-        { y: '2024', a: 16 }
-    ],
-        config = {
-            data: hum,
-            xkey: 'y',
-            ykeys: ['a'],
-            labels: ['Humidity %'],
-            fillOpacity: 0.6,
-            hideHover: 'auto',
-            behaveLikeLine: true,
-            resize: true,
-            pointFillColors: ['#ffffff'],
-            pointStrokeColors: ['#de6fa1'],
-            lineColors: ['#de6fa1'],
-            barColors: ['#de6fa1']
-        };
-    config.element = 'hum-chart';
-    Morris.Line(config);
+        var hum = pre_hum,
+            config = {
+                data: hum,
+                xkey: 'y',
+                ykeys: ['a'],
+                labels: ['Humidity %'],
+                fillOpacity: 0.6,
+                hideHover: 'auto',
+                behaveLikeLine: true,
+                resize: true,
+                pointFillColors: ['#ffffff'],
+                pointStrokeColors: ['#de6fa1'],
+                lineColors: ['#de6fa1'],
+                barColors: ['#de6fa1']
+            };
+        config.element = 'hum-chart';
+        Morris.Line(config);
+        $('#main_temp').append(parseInt(value[value.length-1].t) + "°C");
+        $('#feel-0').append(parseInt(value[value.length-1].hI) + "°C");    
 
-
+    }
+    draw_graphs();
 });
 
 toggle = (idx) => {
