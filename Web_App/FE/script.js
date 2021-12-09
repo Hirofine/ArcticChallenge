ef867a9f5dd4559c2a60498f9e13462d5ff310e9
Hirofine <43345721+Hirofine@users.noreply.github.com>
2021-12-09 09:55:36 +0100

    fully integrated frontend
    just need to change conAdress and ip adress before final deploying

--- Web_App/FE/script.js
+++ Web_App/FE/script.js
@@ -1,18 +1,10 @@
-
-
 $(document).ready(function () {
-    //var Web3 = require('web3');
     let conAddress = "0xEE4fb72da9AeaEc2D8A1E4a21fC9f3E0F507Dd3E"
     let abi = '[{"inputs":[],"name":"getval","outputs":[{"components":[{"internalType":"int256","name":"t","type":"int256"},{"internalType":"uint256","name":"h","type":"uint256"},{"internalType":"int256","name":"hI","type":"int256"},{"internalType":"uint256","name":"timestamp","type":"uint256"}],"internalType":"structCrudApp.Temp[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"components":[{"internalType":"int256","name":"t","type":"int256"},{"internalType":"uint256","name":"h","type":"uint256"},{"internalType":"int256","name":"hI","type":"int256"},{"internalType":"uint256","name":"timestamp","type":"uint256"}],"internalType":"structCrudApp.Temp","name":"recTemp","type":"tuple"}],"name":"setval","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"weatherdb","outputs":[{"internalType":"int256","name":"t","type":"int256"},{"internalType":"uint256","name":"h","type":"uint256"},{"internalType":"int256","name":"hI","type":"int256"},{"internalType":"uint256","name":"timestamp","type":"uint256"}],"stateMutability":"view","type":"function"}]';
-
     let account = "0xd5a84c66d68001aBbe27569A1df9C83D4f1d1F4c";
-
     //const web3 = new Web3("http://172.27.63.201:7545");
     const web3 = new Web3("http://192.168.137.216:7545");
-
     let deploy_contract = new web3.eth.Contract(JSON.parse(abi), conAddress);
-   
-
     let latestKnownBlockNumber = -1;
     let blockTime = 5000;
     var i = 0;
@@ -27,22 +19,17 @@ $(document).ready(function () {
             let transactionReceipt = await web3.eth.getTransactionReceipt(transactionHash);
             transaction = Object.assign(transaction, transactionReceipt);
             console.log("Transaction: ", transaction);
-            // Do whatever you want here
-            //$.getJSON('{"userId": 1, "id": 1,"title": "delectus aut autem","completed": false}', function (data) {
-            //document.getElementById("json").textContent += "<li>" + JSON.stringify(transaction, undefined, 2) + "</li>";
             var li = $('<div class="expandable-item" onclick="toggle(' + i + ')"><div class="expandable-header">Hash: ' +
                 transaction.hash +
                 '<div class="expandable-icon"><div class="line"></div><div class="line"></div></div></div><div class="expandable-body"><div class="container">' +
                 format_division(transaction.blockHash, "Block Hash") + format_division(transaction.blockNumber, "Block Number") + format_division(transaction.from, "From") + format_division(transaction.to, "To")
                 + '</div></div></div>');
-            //li.find('label').text(String(transaction.s));
             $('#json').append(li);
             i++;
-            //});
         }
         latestKnownBlockNumber = blockNumber;
     }
-    
+
 
     // This function is called every blockTime, check the current block number and order the processing of the new block(s)
     async function checkCurrentBlock() {
@@ -51,7 +38,6 @@ $(document).ready(function () {
         for (var i = 0; i <= currentBlockNumber; i++) {
             await processBlock(i);
         }
-        //setTimeout(checkCurrentBlock, blockTime);
     }
 
     checkCurrentBlock()
@@ -63,12 +49,12 @@ $(document).ready(function () {
         var pre_temp = [];
         var pre_hum = [];
         for (var i = 2; i < value.length; i++) {
-            pre_temp[i-2] = { y: parseInt(value[i].timestamp), a: parseInt(value[i].t) };
-            pre_hum[i-2] = {y:parseInt(value[i].timestamp), a:parseInt(value[i].h)}
+            pre_temp[i - 2] = { y: parseInt(value[i].timestamp), a: parseInt(value[i].t) };
+            pre_hum[i - 2] = { y: parseInt(value[i].timestamp), a: parseInt(value[i].h) }
         }
         console.log("Pre temp = ");
         console.log(pre_temp);
-        var temp =pre_temp,
+        var temp = pre_temp,
             config = {
                 data: temp,
                 xkey: 'y',
@@ -103,11 +89,12 @@ $(document).ready(function () {
             };
         config.element = 'hum-chart';
         Morris.Line(config);
-        $('#main_temp').append(parseInt(value[value.length-1].t) + "°C");
-        $('#feel-0').append(parseInt(value[value.length-1].hI) + "°C");    
+        $('#main_temp').append(parseInt(value[value.length - 1].t) + "°C");
+        $('#feel-0').append(parseInt(value[value.length - 1].hI) + "°C");
         var all_data = ""
-        for(var i = 0; i < value.length; i++){
-            all_data += "<ul>" + value[i].t + " | " + value[i].h + " | " + value[i].hI + " | " + value[i].timestamp + "</ul>"
+        for (var i = 0; i < value.length; i++) {
+            all_data += '<div class="row" style="border-style:solid"><div class="col" style="border-right-style: dotted;">' + value[i].t + '</div><div class="col" style="border-left-style: dotted;border-right-style: dotted;">' + value[i].h + '</div><div class="col" style="border-left-style: dotted;border-right-style: dotted;">' + value[i].hI + '</div><div class="col" style="border-left-style: dotted;">' + value[i].timestamp + '</div></div>';
+            //all_data += "<ul><span>" + value[i].t + " </span><span> " + value[i].h + " </span><span> " + value[i].hI + " </span><span> " + value[i].timestamp + "</span></ul>"
         }
         $('#all_data').append(all_data);
     }
